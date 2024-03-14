5be674f9613de2b2c10817e0e9e7f999fe787013
dermo <dermot.logue@dkit.ie>
2024-03-14 15:23:22 +0000

    minor

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -1,2 +1,81 @@
-package org.example;public class Client {
+package org.example;
+
+/**
+ * CLIENT                                                  Mar 2024
+ * <p>
+ * This Client program asks the user to input commands to be sent to the server.
+ * There are only two valid commands in the protocol: "Time" and "Echo"
+ * If user types "time" then that string is sent to the server, and it will respond
+ * by sending back the current time.
+ * <p>
+ * If the user types "echo" followed by a space and a message, the server will echo back the message.
+ * e.g. "echo Nice to meet you".
+ * If the user enters any other input, the server will not understand, and
+ * will send back a message to the effect.
+ * <p>
+ * Note: You must run the server before running this the client.
+ * (Both the server and the client will be running together on this computer)
+ */
+
+import java.io.*;
+import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.start();
+    }
+
+    public void start() {
+
+
+        try (   // create socket to connect to the server
+                Socket socket = new Socket("localhost", 8888);
+                // get the socket's input and output streams, and wrap them in writer and readers
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        ) {
+
+            System.out.println("Client message: The Client is running and has connected to the server");
+
+            //ask user to enter a command
+            Scanner consoleInput = new Scanner(System.in);
+            System.out.println("Valid commands are: \"time\" to get time, or \"echo <message>\" to get message echoed back");
+            System.out.println("Please enter a command: ");
+            String userRequest = consoleInput.nextLine();
+            // send the command to the server on the socket
+            out.println(userRequest);      // write the request to socket along with a newline terminator (which is required)
+           // out.flush();                      // flushing buffer NOT necessary as auto flush is set to true
+
+            // process the answer returned by the server
+            //
+            if (userRequest.startsWith("time"))   // if user asked for "time", we expect the server to return a time (in milliseconds)
+            {
+                String timeString = in.readLine();  // (blocks) waits for response from server, then input string terminated by a newline character ("\n")
+                System.out.println("Client message: Response from server after \"time\" request: " + timeString);
+            }
+            else if( userRequest.startsWith("echo")) // if the user has entered the "echo" command
+            {
+                String response = in.readLine();   // wait for response - expecting it to be the same message that we sent to server
+                System.out.println("Client message: Response from server: \"" + response + "\"");
+            }
+            else
+            {
+                System.out.println("Invalid request or error in response");
+            }
+        } catch (IOException e) {
+            System.out.println("Client message: IOException: " + e);
+        }
+
+        // sockets and streams are closed automatically due to try-with-resources
+
+        System.out.println("Exiting client, but server may still be running.");
+    }
 }
+
+//  LocalTime time = LocalTime.parse(timeString); // Parse String -> convert to LocalTime object if required
--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -1,2 +1,76 @@
-package org.example;public class Server {
+package org.example;
+
+/** SERVER        PROTOCOL                         Mar 2024
+ * Server accepts client connection, reads requests from client,
+ * and sends replies to client, all in accordance with the rules of the protocol.
+ * Server then loops and waits for next request from same client, and continues to loop
+ * until we terminate the server program.
+ * The following PROTOCOL is implemented:
+ * If ( the Server receives the request "time", from a Client )
+ *      then : the server will send back the current time
+ * If ( the Server receives the request "echo message", from a Client )
+ *      then : the server will send back the message
+ * If ( the Server receives the request it does not recognize  )
+ *      then : the server will send back the message "error Sorry, I don't understand"
+ * This is an example of a simple protocol, where the server's response is
+ * based on the client's request.
+ */
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Scanner;
+
+public class Server
+{
+    final int SERVER_PORT_NUMBER = 8888;  // could be any port from 1024 to 49151 (that doesn't clash with other Apps)
+
+    public static void main(String[] args)
+    {
+        Server server = new Server();
+        server.start();
+    }
+
+    public void start()
+    {
+        try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT_NUMBER); )
+        {
+            while(true)
+            {
+                try (   Socket clientSocket = serverSocket.accept();
+                        // connection is made.
+                        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                ) {
+                    System.out.println("Server Message: A Client has connected.");
+
+                    String request = in.readLine(); // wait for input from the client, then read it.
+
+                    System.out.println("Server message: Received from client : \"" + request + "\"");
+
+                    // Implement our PROTOCOL
+                    // The protocol is the logic that determines the responses given based on requests received.
+                    //
+                    if (request.startsWith("time"))  // so, client wants the time !
+                    {
+                        LocalTime time = LocalTime.now();  // get the time
+                        out.println(time);  // send the time to client (as a string of characters)
+                        System.out.println("Server message: time sent to client.");
+                    } else if (request.startsWith("echo")) {
+                        String message = request.substring(5); // strip off the leading substring "echo "
+                        out.println(message);   // send the received message back to the client
+                        System.out.println("Server message: echo message sent to client.");
+                    } else  // an error
+                    {
+                        out.println("error I'm sorry I don't understand your request");
+                        System.out.println("Server message: Invalid request from client.");
+                    }
+                    //out.flush();  // force the response to be sent
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Server Message: IOException: " + e);
+        }
+    }
 }
